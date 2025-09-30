17a4056f5675284ed16db693f7e91b9b20d943ac
paras-hexa <paras@hexacoder.com>
2025-09-30 12:55:34 +0530

    add css in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import { observer } from "mobx-react-lite";
 import { useStore } from "./stores/StoreContext";
 import './App.css'
 const App = observer(() => {
-const store = useStore()
-    const isDark = (store.theme ?? "").toString().toLowerCase() === "dark";
+  const store = useStore()
+  const isDark = (store.theme ?? "").toString().toLowerCase() === "dark";
 
 
   return (
-    <div  style={{ display: "flex", flexDirection: "column", height: "100vh" , backgroundImage: isDark ? "url('./background/background.png')" : "url('./background/whitebg.jpg')" , backgroundSize:"cover" , backgroundRepeat:"no-repeat" }} className="bg-c hide-scrollbar">
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh", backgroundImage: isDark ? "url('./background/background.png')" : "url('./background/whitebg.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="bg-c hide-scrollbar">
       {/* Header */}
       <Header />
 
       {/* Main Content */}
-      <div style={{ display: "flex",  flex: 3 ,}} className="hide-scrollbar overflow-hidden" >
-        {/* Left: Model */}
+      <div
+        className="flex flex-col lg:flex-row hide-scrollbar overflow-hidden flex-1"
+        style={{ minHeight: 0 }}
+      >        {/* Left: Model */}
         <ModelViewer />
 
         {/* Right: Sidebar */}
